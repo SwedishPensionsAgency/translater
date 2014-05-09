f71e9590fc0ba0ff48846e677537c9bc5e1d6b18
Cédric Perriard <cedric.perriard@pensionsmyndigehten.se>
2014-05-09 17:10:04 +0200

    added new environment in .onLoad

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,4 @@
-.onAttach <- function(libname, pkgname){
-   
+.onLoad <- function (libname, pkgname) {
+  #Create a environment in the package environment for storing the translation tables 
+  assign(".tables.env", new.env(), envir= asNamespace(pkgname))
 }
