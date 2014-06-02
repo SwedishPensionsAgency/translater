38fdd268d23a9e496fee8e8a34fc9cc74f5bb516
Cédric Perriard <cedric.perriard@pensionsmyndigehten.se>
2014-06-02 16:56:36 +0200

    check if table for missing translation exists

--- R/translation-table-functions.R
+++ R/translation-table-functions.R
@@ -73,7 +73,7 @@ is_in_missing_translations <- function (string, source.language, object.name) {
 #' @family translation table
 get_missing_translations <- function (delete = FALSE) {
   return.value <- .tables.env$missing.translations
-  if (delete) {
+  if (delete && exists("missing.translations", envir = .tables.env)) {
     rm(missing.translations, envir = .tables.env)
   }
   return(return.value)
