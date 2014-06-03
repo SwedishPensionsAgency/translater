912c7fc902bd2af27dd0fec37f798ad5f3a98157
Cédric Perriard <cedric.perriard@pensionsmyndigehten.se>
2014-06-03 16:23:12 +0200

    fixed problem with some lists, e.g. ggplot-lists, with non standard classes

--- R/get-element.R
+++ R/get-element.R
@@ -6,8 +6,17 @@
 #' @param name A literal character string or a name (possibly backtick quoted) or an index
 #' @export
 get_element <- function (object, name) {
+  if (typeof(object) == "list") {
+    class(object) <- "list"
+  }
   if (is.environment(object) && !.hasSlot(object, name)) {
     return(get(name, envir = object))
+  } else if (is.numeric(name)) {
+    if (isS4(object)) {
+      return(slot(object, name))
+    } else {
+      return(object[[name]])
+    }
   } else {
     return(getElement(object, name))
   }
